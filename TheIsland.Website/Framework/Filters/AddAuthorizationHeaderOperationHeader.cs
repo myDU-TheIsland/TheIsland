c686341e7d6cb6a5488fafbd4a970314ca3e1d4e
Paul Layne <me@thearchitect.io>
2024-11-05 14:35:33 -0500

    added auth tag just incase

--- TheIsland.Website/Framework/Filters/AddAuthorizationHeaderOperationHeader.cs
+++ TheIsland.Website/Framework/Filters/AddAuthorizationHeaderOperationHeader.cs
@@ -15,25 +15,25 @@ namespace TheIsland.Website.Framework.Filters
         public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
             var actionMetadata = context.ApiDescription.ActionDescriptor.EndpointMetadata;
-            var isAuthorized = actionMetadata.Any(metadataItem => metadataItem is ApiKeyAttribute);
+            var isAPIKey = actionMetadata.Any(metadataItem => metadataItem is ApiKeyAttribute);
+            var isAuthorize = actionMetadata.Any(metadataItem => metadataItem is AuthorizeAttribute);
             var allowAnonymous = actionMetadata.Any(metadataItem => metadataItem is AllowAnonymousAttribute);
 
-            if (!isAuthorized || allowAnonymous)
+            if ((!isAPIKey && !isAuthorize) || allowAnonymous)
             {
                 return;
             }
 
-            if (operation.Parameters == null)
-            {
-                operation.Parameters = new List<OpenApiParameter>();
-            }
+            operation.Parameters ??= new List<OpenApiParameter>();
 
             operation.Security = new List<OpenApiSecurityRequirement>();
 
             operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
             operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden" });
 
-            operation.Security.Add(new OpenApiSecurityRequirement
+            if (isAPIKey)
+            {
+                operation.Security.Add(new OpenApiSecurityRequirement
                 {
                     {
                         new OpenApiSecurityScheme
@@ -43,6 +43,7 @@ namespace TheIsland.Website.Framework.Filters
                         Array.Empty<string>()
                     },
                 });
+            }
         }
     }
 }
