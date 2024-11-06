d5625dd884e4e0eb5df68d141de7306393bc9e01
Lex <55722511+LexLeach@users.noreply.github.com>
2024-11-06 12:56:05 -0700

    website redesign(wip)

---
+++ TheIsland.Website/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./**/*.cshtml', './Pages/**/*.cshtml', './Views/**/*.cshtml'],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: '#23283e',
+        midnightBlue: '#1a1a2e',
+      },
+      spacing: {
+        30: '7rem',
+        35: '7.5rem',
+        100: '28rem',
+      },
+      transformOrigin: {
+        'top-left': 'top left',
+      },
+    },
+  },
+  plugins: [],
+};
