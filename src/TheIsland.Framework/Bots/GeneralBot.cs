f7454322da88dc70fa9464f72a6f32e24cd738f6
Paul Layne <me@thearchitect.io>
2025-03-07 18:15:05 -0500

    fixed queryh

--- src/TheIsland.Framework/Bots/GeneralBot.cs
+++ src/TheIsland.Framework/Bots/GeneralBot.cs
@@ -150,15 +150,6 @@ namespace TheIsland.Framework.Bots
 
                     if (transactions.Count() > 0)
                     {
-                        if (transactions.Count() > 1)
-                        {
-                            var deduct = -((transactions.Count() - 1) * amount);
-                            var playerWallet = await this._dualPlayerRepository.GetAsync(player.id).ConfigureAwait(false);
-                            await this.DataAccessor.PlayerDisconnectAsync(Convert.ToUInt64(player.id)).ConfigureAwait(false);
-                            await this._dualPlayerRepository.UpdateWallet(player.id, deduct).ConfigureAwait(false);
-                            logMessage(@$"Deducting from user '{player.display_name}', got multiple payments.");
-                        }
-
                         //transaction in the last hour
                         logMessage(@$"Skipping user '{player.display_name}', w allet has transaction in the last hour({hours}).");
                         continue;
